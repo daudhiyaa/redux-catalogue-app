01c8034e1075ab60e7293de64e78282329bad92b
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-12 23:36:36 +0700

    feat: add Route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" Component={ProductListing} />
+          <Route path="/product/:productId" Component={ProductDetail} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
