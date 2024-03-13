23cc11516dd7e6481d9975f142830ba47f7379f4
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-13 16:14:57 +0700

    feat: reducer for selectedProduct

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
-import { productReducer } from "./productReducer";
+import { combineReducers } from 'redux';
+import { productReducer, selectedProductReducer } from './productReducer';
 
 const reducers = combineReducers({
+  // * key: reducer
   allProducts: productReducer,
+  product: selectedProductReducer,
 });
 
 export default reducers;
